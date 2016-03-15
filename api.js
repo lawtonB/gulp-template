4adc053d98cb7cc8684fdd803e8630dd881c29b5
lawton browning and tim durward <student@epicodus.com>
2016-03-15 08:45:45 -0700

    add api key and weather api

---
+++ api.js
@@ -0,0 +1,14 @@
+//api key is stored in .env (importing a key increases security - don't forget to gitignore file)
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.message);
+    });
+  });
+});
